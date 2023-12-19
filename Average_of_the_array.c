5ab9899bd7bfa6097eec667fe7ef37e0880134a4
yaaswith <147487553+yaaswith@users.noreply.github.com>
2023-12-19 14:35:13 +0530

    #include<stdio.h> int main() {     int n,i;     float avg,sum=0;     scanf("%d",&n);     int arr[n];     for (i=0;i<n;i++)     {         scanf("%d",&arr[n]);         sum=sum+arr[n];         avg=sum/n;     }      printf("%.2f",avg);
    }

---
+++ Average_of_the_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    float avg,sum=0;
+    scanf("%d",&n);
+    int arr[n];
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&arr[n]);
+        sum=sum+arr[n];
+        avg=sum/n;
+    }
+     printf("%.2f",avg);
+    
+}
